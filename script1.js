9dd65f89e8955eafd8ec8c4ee6d2936ebe30fbe0
chloe_choi <plumxeun@gmail.com>
2024-03-12 21:26:54 +0900

    0312 langing page revise

--- script1.js
+++ script1.js
@@ -1,3 +1,20 @@
+//메뉴 스크롤 시 픽스하기
+var menu = document.querySelector('.menu');
+
+var menuOffset = menu.offsetTop;
+
+function fixMenu() {
+  if (window.scrollY >= menuOffset) {
+    menu.classList.add('fixed');
+  } else {
+    menu.classList.remove('fixed');
+  }
+}
+
+window.addEventListener("scroll", fixMenu);
+
+
+//메뉴 글씨 바꾸기
 function changeText(isHover) {
     var link = document.querySelector('.github');
     if (isHover) {
@@ -5,4 +22,22 @@ function changeText(isHover) {
     } else {
       link.textContent = 'CHLOE CHOI';
     }
-  }
+}
+
+
+let observer = new IntersectionObserver((e)=>{
+    e.forEach((box, index)=>{
+        if(box.intersectionRatio){
+            box.target.style.opacity = 1;
+        } else{
+            box.target.style.opacity= 0;
+        }
+    })
+})
+let section = document.querySelectorAll('.section')
+observer.observe(section[0])
+observer.observe(section[1])
+observer.observe(section[2])
+observer.observe(section[3])
+observer.observe(section[4])
+observer.observe(section[5])
